1d8d66741a9965f80c3d8f85e42d0d9f3c0c4083
Austin McPhail <austin@mcphail.dev>
2019-12-05 06:04:12 -0600

    feat: added more intelligent phrase finding and more matches

--- notybot.js
+++ notybot.js
@@ -1,5 +1,24 @@
 const Bot = require('keybase-bot');
 
+const THANK_SET = 0;
+const F_SET = 1;
+
+const PHRASE_SETS = [
+    ['thank you', ' ty ', 'thanks', 'thank', 'merci', 'gracias'],
+    ['fuck you']
+];
+
+function findMatch(text) {
+    let match = null;
+    PHRASE_SETS.forEach((set, index) => {
+        const s = set.findIndex(p => text.toLowerCase().includes(p));
+        if (s !== -1) {
+            match = [index, s];
+        }
+    });
+    return match;
+}
+
 async function main() {
     const bot = new Bot()
     try {
@@ -19,12 +38,27 @@ async function main() {
                 return;
             }
 
-            if (!body.toLowerCase().includes('thank you')) {
+            if (body.includes(`@${bot.myInfo().username}`)) {
+                bot.chat.send(channel, { body: `Hey @here, look at big brain @${username} talking to a bot!` });
+                return;
+            }
+
+            match = findMatch(body);
+            if (match == null) {
                 console.info('Message does not contain key phrase.');
                 return;
             }
 
-            bot.chat.send(channel, { body: `No, thank YOU @${username}!` });
+            switch (match[0]) {
+                case THANK_SET:
+                    let m = !body.includes('you') || !PHRASE_SETS[match[0]][match[1]].includes('you') ? `No, ${PHRASE_SETS[match[0]][match[1]].trim()} to you @${username}!` : `No, ${PHRASE_SETS[match[0]][match[1]].trim()} @${username}!`;
+                    m = m.replace(' you ', ' YOU ');
+                    bot.chat.send(channel, { body: m });
+                    break;
+                case F_SET:
+                    bot.chat.send(channel, { body: `No, fuck YOU @${username}!` });
+                    break;
+            }
         };
 
         console.log(`Notybot initialized as ${bot.myInfo().username}.`);
